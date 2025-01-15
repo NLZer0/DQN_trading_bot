23891bda29b82e92d03d6a0f9f8e1f47ef47c25c
Zmitrovich Nikolay <nikolayzmi@bk.ru>
2025-01-15 18:12:59 +0700

    train model with big comission

--- dqn/config.py
+++ dqn/config.py
@@ -28,11 +28,11 @@ class Config:
     log_interval: int = 10
     data_path: str = '/home/nikolayz/Рабочий стол/RL_research/data/sber_1h_labeled.csv'
     model_path: str = 'saved_models/qnet/model'
-    test_name: str = 'rainbow_4'
+    test_name: str = 'rainbow_4_big_comission'
 
     max_buffer_size: int = 30_000
     batch_size: int = 1024
-    comission: float = 0.0005
+    comission: float = 0.002
     initial_balance: float = 10_000       
     window_size: int = 16
     memory_capacity: int = 10_000
