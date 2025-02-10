65a058e08269138dd4bc3b4c3e1ef51f59b9732f
Zmitrovich Nikolay <nikolayzmi@bk.ru>
2025-02-10 11:33:42 +0700

    change_config.py

--- dqn/config.py
+++ dqn/config.py
@@ -17,23 +17,24 @@ class Config:
 
     ema_span = 5 # коэффициент для вычисления ema
     random_eps = 0.5 # коэффициент случайных действий
-    random_eps_scaler = 0.8 # коэффициент затухания случайных действий
-    gamma: float = 0.99  # дисконтирование награды
+    random_eps_scaler = 0.5 # коэффициент затухания случайных действий
+    gamma: float = 0.95  # дисконтирование награды
     beta: float = 0.5
     lr: float = 1e-3 # скорость обучения
     # epochs: int = 10  # количество эпох для обновления политики
     device: str = 'cuda'     
     
     silent: bool = False
-    num_episodes: int = 50
-    log_interval: int = 5
+    num_episodes: int = 10
+    log_interval: int = 1
+    is_short: bool = True
     data_path: str = '/home/nikolayz/Рабочий стол/RL_research/data/btc_labeled.csv'
     model_path: str = 'saved_models/qnet/model'
-    test_name: str = 'rainbow_4_bc32_btc_big-train-test'
+    test_name: str = 'rainbow_4_bc32_btc_sw-long-short'
 
     max_buffer_size: int = 10_000
     batch_size: int = 1024
-    comission: float = 0.002
+    comission: float = 0.001
     initial_balance: float = 1_000_000       
     window_size: int = 32
     memory_capacity: int = 10_000
